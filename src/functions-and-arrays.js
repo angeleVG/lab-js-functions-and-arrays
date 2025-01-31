29fcca79ed15d1242b37fc49841623f5bcd48ec9
Angèle van Groesen <a.vangroesen@outlook.com>
2025-01-31 21:21:01 +0100

    Solved lab

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,34 +1,51 @@
 // Iteration 1 | Find the Maximum
-function maxOfTwoNumbers() {}
-
-
+function maxOfTwoNumbers(num1, num2) {
+    return Math.max(num1, num2);
+}
+console.log(maxOfTwoNumbers(15, 8));
 
 
 // Iteration 2 | Find the Longest Word
 const words = ["mystery", "brother", "aviator", "crocodile", "pearl", "orchard", "crackpot"];
 
-function findLongestWord() {}
+function findLongestWord(words) {
+        if (words.length === 0) {
+          return null;
+        }
+    }
 
-
-
-
+    
 // Iteration 3 | Sum Numbers
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
-function sumNumbers() {}
-
+function sumNumbers(numbers) {
+ let sum = 0;
+  
+ for (let i = 0; i < numbers.length; i++) {
+   sum += numbers[i];
+ }
+ return sum
+    }
 
 
 
 // Iteration 4 | Numbers Average
 const numbers2 = [2, 6, 9, 10, 7, 4, 1, 9];
 
-function averageNumbers() {}
-
-
+function averageNumbers(numbers2) {}
 
 
 // Iteration 5 | Find Elements
 const words2 = ["machine", "subset", "trouble", "starting", "matter", "eating", "truth", "disobedience"];
 
-function doesWordExist() {}
+function doesWordExist(wordsArray, wordsToFind) {
+    if (wordsArray.length === 0) {
+        return null;
+}
+else if (wordsArray.indexOf === undefined){
+    return: false;
+}
+else {
+    return true
+}
+}
